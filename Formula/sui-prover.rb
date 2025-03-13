2898bd91baee85a6f5ec342f6374c91e3645d57e
Andrii <andrii@asymptotic.tech>
2025-03-13 18:53:14 +0200

    sui-prover 0.1.0 (new formula)
    Closes #1.
    
    Signed-off-by: andrii-a8c <202856126+andrii-a8c@users.noreply.github.com>

---
+++ Formula/sui-prover.rb
@@ -0,0 +1,57 @@
+class SuiProver < Formula
+  desc "Prover - a tool for verifying Move smart contracts on the Sui blockchain"
+  homepage "https://github.com/asymptotic-code/sui"
+  license "Apache-2.0"
+
+  stable do
+    depends_on "dotnet@8"
+    url "https://github.com/asymptotic-code/sui.git", branch: "next"
+    version "0.1.0"
+    resource "boogie" do
+      url "https://github.com/boogie-org/boogie.git", branch: "master"
+    end
+  end
+
+  head "https://github.com/asymptotic-code/sui.git", branch: "next" do
+    depends_on "dotnet@8"
+    resource "boogie" do
+      url "https://github.com/boogie-org/boogie.git", branch: "master"
+    end
+  end
+
+  depends_on "rust" => :build
+  depends_on "z3"
+
+  def install
+    system "cargo", "install", "--locked", "--path", "./crates/sui-move", "--features", "all"
+    # system "cargo", "build", "--release", "--features", "build", "--package", "sui-move"
+
+    libexec.install "target/release/sui-move"
+
+    ENV.prepend_path "PATH", Formula["dotnet@8"].opt_bin
+    ENV["DOTNET_ROOT"] = Formula["dotnet@8"].opt_libexec
+
+    resource("boogie").stage do
+      system "dotnet", "build", "Source/Boogie.sln", "-c", "Release"
+      libexec.install Dir["Source/BoogieDriver/bin/Release/net8.0/*"]
+      bin.install_symlink libexec/"BoogieDriver" => "boogie"
+    end
+
+    (bin/"sui-move").write_env_script libexec/"sui-move", {
+      DOTNET_ROOT: Formula["dotnet@8"].opt_libexec,
+      BOOGIE_EXE:  bin/"boogie",
+      Z3_EXE:      Formula["z3"].opt_bin/"z3",
+    }
+  end
+
+  def caveats
+    <<~EOS
+      The formal verification toolchain has been installed.
+    EOS
+  end
+
+  test do
+    system "z3", "--version"
+    system "#{bin}/sui-move", "--version"
+  end
+end
