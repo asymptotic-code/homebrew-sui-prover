75f6926d6a91354781371e00a1e4a51b48718ad7
andrii-a8c <202856126+andrii-a8c@users.noreply.github.com>
2025-11-21 18:36:35 +0000

    sui-prover: update 0.3.109 bottle.

--- Formula/sui-prover.rb
+++ Formula/sui-prover.rb
@@ -13,9 +13,9 @@ class SuiProver < Formula
   end
 
   bottle do
-    root_url "https://github.com/asymptotic-code/homebrew-sui-prover/releases/download/sui-prover-0.3.108"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea566d5dd20c653b84221a333cb02e89db794328d97f927306f5c533244089cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4adeb2b7a3eb8bbb2e6f3d4e5653a404cccfaabe1ffe28cdcb4ddd4f57cf748e"
+    root_url "https://github.com/asymptotic-code/homebrew-sui-prover/releases/download/sui-prover-0.3.109"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "933c88c513c6b5278c395a120e02fd955df037d532c032fd194df5549ce4cc9b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fceff9ea7ce12ed4caf021111d0d0a33178ecc28e26c2bdc263b4fa6c9b14be"
   end
 
   head "https://github.com/asymptotic-code/sui-prover.git", branch: "main" do
